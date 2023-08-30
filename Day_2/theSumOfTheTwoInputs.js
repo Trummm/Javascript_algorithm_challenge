cc6bf3d5a5852e50e2556439cc42976d5cd683fa
Trummm <trungpro2000.tp2@gmail.com>
2023-08-30 09:56:44 +0700

    day 2

---
+++ Day_2/theSumOfTheTwoInputs.js
@@ -0,0 +1,11 @@
+const theSumOfTheAllInputs = (args) => {
+  const result = args.reduce((init, item) => {
+    return init + item
+  }, 0)
+
+  return result
+}
+
+const theSumOfTheTwoInputs = (a, b) => {
+  return a + b
+}
