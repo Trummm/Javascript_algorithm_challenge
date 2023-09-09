86a056971222319de42f5e216032e38ec79abc3b
Trummm <trungpro2000.tp2@gmail.com>
2023-09-09 20:54:46 +0700

    day 7

---
+++ Day_7/alphabetSubSequence.js
@@ -0,0 +1,8 @@
+const alphabetSubSequence = (s) => {
+  var isSubsequence = true
+  for (var i = 1; i < s.length; i++) {
+    if (s.charCodeAt(i) <= s.charCodeAt(i - 1)) isSubsequence = false
+  }
+
+  return isSubsequence
+}
