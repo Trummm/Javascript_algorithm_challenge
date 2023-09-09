878205397e47b1c15256697b348755cc99d5f362
Trummm <trungpro2000.tp2@gmail.com>
2023-09-09 20:57:54 +0700

    update

---
+++ Day_8/alphabeticShift.js
@@ -0,0 +1,8 @@
+const alphabeticShift = (inputString) => {
+  return inputString
+    .split('')
+    .map((c) =>
+      c.charCodeAt(0) === 122 ? 'a' : String.fromCharCode(c.charCodeAt(0) + 1)
+    )
+    .join('')
+}
