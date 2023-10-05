f42ea9d6edc322d4e5bd612c7166e7ed69474b80
Trummm <trungpro2000.tp2@gmail.com>
2023-10-05 15:43:43 +0700

    day 11

---
+++ Day_11/areSimilar.js
@@ -0,0 +1,20 @@
+const areSimilar = (a, b) => {
+  var are = true
+  var av = null
+  var bv = null
+  var swap = false
+  for (var i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      if (av === null || bv === null) {
+        av = a[i]
+        bv = b[i]
+      } else {
+        if (swap || av !== b[i] || bv !== a[i]) {
+          are = false
+        }
+        swap = true
+      }
+    }
+  }
+  return are
+}
