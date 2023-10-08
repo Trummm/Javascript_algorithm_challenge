2228ea7d42126a4bf9fe7f607910b716e1c6fa5c
Trummm <trungpro2000.tp2@gmail.com>
2023-10-08 11:27:26 +0700

    day 12

---
+++ Day_13/arrayConversion.js
@@ -0,0 +1,16 @@
+const arrayConversion = (arr) => {
+  var newArr = []
+  var j = 0
+  while (arr.length > 1) {
+    newArr = []
+    for (let i = 0; i < arr.length; i += 2) {
+      newArr.push(arr.slice(i, i + 2))
+    }
+    arr = newArr.map((pair) => (j ? pair[0] * pair[1] : pair[0] + pair[1]))
+    j = (j + 1) % 2
+  }
+
+  return arr[0]
+}
+
+console.log(arrayConversion([1, 2, 3, 4, 5, 6, 7, 8]))
